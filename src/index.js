bb00a271ca40120303c15cd93c3009e1aa14d798
vis0912 <vishalmpranav2003@gmail.com>
2024-04-11 23:41:33 +0530

    utils folder talk

--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import connectDB  from "./db/index.js";
+import {app} from '../src/app.js';
 
 dotenv.config({
     path: "./env"
 });
 
 connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, ()=>{
+      console.log(`App listening at http://localhost:${process.env.PORT || 8000}`);
+    })
+  })
+  .catch((err)=>{
+    console.log("Mongo Connection FAILED!!",err);
+    process.exit(1);
+  })
