8ea8e26d72101f935b50201cac636cf6c1597f91
vis0912 <vishalmpranav2003@gmail.com>
2024-04-13 22:52:34 +0530

    commit

---
+++ src/models/video.models.js
@@ -0,0 +1,42 @@
+import mongoose, {Schema} from "mongoose";
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+
+const videoSchema= new Schema({
+    videoFile: {
+        type: String,   //cloudinary url
+        required: true,
+    },
+    thumbnail: {
+        type: String,   //cloudinary url
+        required: true,
+    },
+    title: {
+        type: String,
+        required: true,
+    },
+    description: {
+        type: String,
+        required: true,
+    },
+    duration: {
+        type: Number,   //cloudinary url
+        required: true,
+    },
+    views: {
+        type: Number,   //cloudinary url
+        default: 0,
+    },
+    isPublished: {
+        type: Boolean,   //cloudinary url
+        default: true,
+    },
+    owner: {
+        type: Schema.Types.ObjectId,   //cloudinary url
+        ref:"User",
+    },
+
+},{timestamps:true});
+
+videoSchema.plugin(mongooseAggregatePaginate);
+
+export const Video = mongoose.model( "Video", videoSchema );
